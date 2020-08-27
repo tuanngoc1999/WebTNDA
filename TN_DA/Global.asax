3ccd956b950d2931dcfeaa00dd18ef15e856aad0
User <User@MAITUAN>
2020-08-27 14:34:30 +0700

    Add project files.

---
+++ TN_DA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TN_DA.MvcApplication" Language="C#" %>
---
+++ TN_DA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TN_DA.Startup))]
+namespace TN_DA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
